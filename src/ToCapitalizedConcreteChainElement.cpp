6a5717da2d1cd99f5bb1f342483094daa9b028c4
Figurante17 <amadeu_carvalho@live.com>
2017-06-23 21:29:13 -0300

    Add files via upload

---
+++ src/ToCapitalizedConcreteChainElement.cpp
@@ -0,0 +1,33 @@
+#include <cctype>
+#include "ToCapitalizedConcreteChainElement.hpp"
+
+ToCapitalizedConcreteChainElement::ToCapitalizedConcreteChainElement()
+   {
+   this->setNext(NULL);
+   }
+
+ToCapitalizedConcreteChainElement::~ToCapitalizedConcreteChainElement()
+   {
+   this->setNext(NULL);
+   }
+
+string ToCapitalizedConcreteChainElement::getName()
+   {
+   return (this->name);
+   }
+
+void ToCapitalizedConcreteChainElement::internalProcessing(MyDataObject* value)
+   {
+   string buffer = value->getValue();
+   
+   for(int count = 0; count < buffer.size(); count++){
+         if (count == 0){
+            buffer.at(count) = toupper(buffer.at(count));   
+         }else if(buffer.at(count-1) == ' '){
+            buffer.at(count) = toupper(buffer.at(count));   
+         }else{
+            buffer.at(count) = tolower(buffer.at(count));
+         }
+   }
+   value->setValue(buffer);
+   }
